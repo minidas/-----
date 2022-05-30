aad692b8020bf60a8731be86ce2174c6ae291193
minidas <2000011346@stu.pku.edu.cn>
2022-05-30 20:26:29 +0800

    v0.5 加入了对于柱式和弦的分析，并优化了对于停止符的处理

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
+import numpy as py
 import  music21 as ms21
 c=ms21.converter.parse('稻妻-褪淡的余忆_chord.mid')
 m=ms21.converter.parse('稻妻-褪淡的余忆_medoly.mid')
@@ -27,6 +28,7 @@ while True:
                         MR[0].append(m_note.offset-t)
                         MR[1].append(m_note.duration.quarterLength)
                     m_note=next(mrit)
+                temp=note.offset-note.offset%2-t
                 t=note.offset-note.offset%2
                 f=1
                 id=0
@@ -34,17 +36,23 @@ while True:
                     if (se[0]==CR):
                         f=0
                         se[1].append(MR)
+                        se[3].append(temp)
                         break
                     id+=1
                 if(f):
-                    R.append([CR,[MR],[]])
+                    R.append([CR,[MR],[],[temp]])
                 if(fstart):
                     R[pre_id][2].append(id)
                 fstart=1
                 pre_id=id
                 CR=[[],[]]
-            CR[0].append(note.offset-t)
-            CR[1].append(note.duration.quarterLength)
+            if(isinstance(note,ms21.note.Note)):
+                CR[0].append(note.offset-t)
+                CR[1].append(note.duration.quarterLength)
+            else:
+                for i in note.notes:
+                    CR[0].append(note.offset-t)
+                    CR[1].append(note.duration.quarterLength)
     except StopIteration:
         break
 crit=iter(c.flat.notesAndRests)
@@ -54,7 +62,7 @@ C=[]
 t=0
 pre_id=0
 fstart=0
-Cpitch=[]
+Cstep=py.linspace(0,0,12)
 m_note=next(mrit)
 while True:
     try:
@@ -66,27 +74,38 @@ while True:
                     if (isinstance(m_note,ms21.note.Note) or isinstance(m_note,ms21.chord.Chord)):
                         if(m_note.offset>=note.offset-note.offset%2):#对应chord分段
                             break
-                        M.append(m_note.pitch.midi)
+                        if(isinstance(m_note,ms21.note.Note)):
+                            M.append(m_note.pitch.midi)
+                        else:
+                            for i in m_note.notes:
+                                M.append(i.pitch.midi)
                     m_note=next(mrit)
                 t=note.offset-note.offset%2
                 f=1
                 id=0
+                bi_Cstep=0#Cstep数组的二进制表示
+                for i in range(12):
+                    if(Cstep[i]):
+                        bi_Cstep+=1
+                    bi_Cstep<<=1
+                bi_Cstep>>=1
                 for se in C:
-                    if (se[0]==Cpitch):
+                    if (se[0]==bi_Cstep):
                         f=0
                         se[1].extend(M)
                         break
                     id+=1
                 if(f):
-                    C.append([Cpitch,M,[]])
+                    C.append([bi_Cstep,M,[]])
                 if(fstart):
                     C[pre_id][2].append(id)
                 fstart=1
                 pre_id=id
-                Cpitch=[]
-            Cpitch.append(note.pitch.midi)
+                Cstep=py.linspace(0,0,12)
+            if(isinstance(note,ms21.note.Note)):
+                Cstep[note.pitch.midi%12]=1
+            else:
+                for i in note.notes:
+                    Cstep[i.pitch.midi%12]=1
     except StopIteration:
         break
-
-
-
