691e0b730228088905c757ceffdb8b127b60f6a3
minidas <2000011346@stu.pku.edu.cn>
2022-05-30 15:08:41 +0800

    分离了和弦的节奏

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ while True:
         note=next(crit)
         if (isinstance(note,ms21.note.Note) or isinstance(note,ms21.chord.Chord)):
             if(note.offset>=t+2):
-                t+=2
+                t=note.offset-note.offset%2
                 f=1
                 for se in R:
                     if (se[0]==CR):f=0
                 if(f):
                     R.append([CR])
-                CR=[]
+                CR=[[],[]]
             CR[0].append(note.offset-t)
             CR[1].append(note.duration.quarterLength)
     except StopIteration:
         break
-    if isinstance(note, ms21.note.Rest):
-        print(note.offset,note.name,note.duration.quarterLength)
+if isinstance(note, ms21.note.Rest):
+    print(note.offset,note.name,note.duration.quarterLength)
 
-    elif isinstance(note,ms21.note.Note):
-        print(note.offset,note.name,note.octave,note.pitch,note.pitch.midi,note.duration.quarterLength)
+elif isinstance(note,ms21.note.Note):
+    print(note.offset,note.name,note.octave,note.pitch,note.pitch.midi,note.duration.quarterLength)
     # 取和弦
-    else:
-        for c_note in note.notes:
-            print(c_note.name,c_note.pitch.midi)
-        print(note.offset,note.duration.quarterLength)
+else:
+    for c_note in note.notes:
+        print(c_note.name,c_note.pitch.midi)
+    print(note.offset,note.duration.quarterLength)
 
 
